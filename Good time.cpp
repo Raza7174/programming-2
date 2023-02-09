0ce4d852c9fd857fe8c6e8bc3f7e195ce7f80951
Jaon Raza <124121326+Raza7174@users.noreply.github.com>
2023-02-09 09:42:22 +0500

    Add files via upload

---
+++ Good time.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int time=12;
+if(time<03)
+{
+cout<<"it is good morning";
+}
+else if (time>10)
+{
+cout<<"it is good afternoon";
+}
+else
+{
+cout<<"it is evening";
+}
+return 0;
+}
+    
